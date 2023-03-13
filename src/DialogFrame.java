f129e33338c1a0f3a60de5f8a586aa16b6b67365
Nazar <nazarmozol@mail.ru>
2023-03-13 15:25:35 +0300

    Fix dialog frame

--- src/DialogFrame.java
+++ src/DialogFrame.java
@@ -48,13 +48,13 @@ public class DialogFrame extends JFrame {
 		setTitle(user.getName() + "'s private chat");
 		setMinimumSize(new Dimension(WIDTH, HEIGHT));
 		final Toolkit kit = Toolkit.getDefaultToolkit();
-		setLocation((kit.getScreenSize().width - getWidth())/2,
-				(kit.getScreenSize().height - getHeight())/2);
+		setLocation((kit.getScreenSize().width - getWidth()),
+				(kit.getScreenSize().width - getHeight()));
 
 		textAreaIn = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
 		textAreaIn.setEditable(false);
 		final JScrollPane scrollPaneIncoming = new JScrollPane(textAreaIn);
-		textAreaOut = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+		textAreaOut = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
 		final JScrollPane scrollPaneOutgoing = new JScrollPane(textAreaOut);
 		final JPanel messagePanel = new JPanel();
 		messagePanel.setBorder(BorderFactory.createTitledBorder("Message"));
@@ -84,14 +84,14 @@ public class DialogFrame extends JFrame {
 				.addContainerGap());
 
 		final GroupLayout layout1 = new GroupLayout(getContentPane());
-		setLayout(layout1);
-		layout1.setHorizontalGroup(layout1.createSequentialGroup()
+		setLayout(layout2);
+		layout2.setHorizontalGroup(layout2.createSequentialGroup()
 				.addContainerGap()
-				.addGroup(layout1.createParallelGroup()
+				.addGroup(layout2.createParallelGroup()
 						.addComponent(scrollPaneIncoming)
 						.addComponent(messagePanel))
 				.addContainerGap());
-		layout1.setVerticalGroup(layout1.createSequentialGroup()
+		layout2.setVerticalGroup(layout2.createSequentialGroup()
 				.addContainerGap()
 				.addComponent(scrollPaneIncoming)
 				.addGap(MEDIUM_GAP)
